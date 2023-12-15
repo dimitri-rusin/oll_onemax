b5ae3086d60a2e8ba82a832c8e907eb8c16acd0c
Dimitri Rusin <dimitri@habimm.com>
2023-12-15 17:27:06 +0100

    Code zum Schreiben in .CSV.

--- env.py
+++ env.py
@@ -8,11 +8,14 @@ class OLL_OneMax(gymnasium.Env):
 
   def __init__(
     self,
+    file = None,
     num_dimensions = 5,
     optimum = None,
     random_seed = None,
   ):
     numpy.random.seed(random_seed)
+
+    self.file = file
 
     self.num_dimensions = num_dimensions
     self.optimum = numpy.random.randint(0, 2, size = self.num_dimensions, dtype = numpy.int32)
@@ -40,6 +43,8 @@ class OLL_OneMax(gymnasium.Env):
     next_random_seed = numpy.random.randint(0, 10000)
     super().reset(seed = next_random_seed)
 
+    self.render()
+
     info = {}
 
     return self.current_fitness, info
@@ -54,46 +59,58 @@ class OLL_OneMax(gymnasium.Env):
       assignment_from_first_mutation = self.assignment.copy()
       dices = numpy.random.randint(0, self.num_dimensions, size = self.num_dimensions, dtype = numpy.int32)
       assignment_from_first_mutation = numpy.where(dices == 0, 1 - assignment_from_first_mutation, assignment_from_first_mutation)
-
       new_fitness = self.onemax(assignment_from_first_mutation)
-      print(self.assignment)
-      print(self.current_fitness)
-      print(assignment_from_first_mutation)
-      print(new_fitness)
-      breakpoint()
-
       if best_fitness_from_first_mutation is None or new_fitness >= best_fitness_from_first_mutation:
         best_assignment_from_first_mutation = assignment_from_first_mutation
         best_fitness_from_first_mutation = new_fitness
 
+    best_assignment_from_second_mutation = self.assignment
+    best_fitness_from_second_mutation = self.current_fitness
+
     if self.current_fitness <= best_fitness_from_first_mutation >= best_fitness_from_second_mutation:
       self.current_fitness = best_fitness_from_first_mutation
-      self.assignment = best_fitness_from_first_mutation
-    elif self.best_fitness_from_second_mutation >= self.current_fitness:
+      self.assignment = best_assignment_from_first_mutation
+    elif best_fitness_from_second_mutation >= self.current_fitness:
       self.current_fitness = best_fitness_from_second_mutation
       self.assignment = best_assignment_from_second_mutation
 
-    terminated = self.current_fitness == self.num_dimensions
+    terminated = self.current_fitness == 1
     reward = self.current_fitness - previous_fitness
     info = {}
 
-    return self.current_fitness, reward, terminated, info
+    self.render()
+
+    return self.current_fitness, reward, terminated, False, info
 
   def render(self):
-   return self._render_frame()
 
-  def _render_frame(self):
-   return "Hello"
+    print(
+      self.assignment,
+      '|',
+      self.current_fitness,
+      file = self.file,
+      sep = ''
+    )
 
   def close(self):
    pass
 
 
+
 if __name__ == '__main__':
-  oll_onemax = OLL_OneMax()
-  print("oll_onemax.optimum")
-  print(oll_onemax.optimum)
-  print("=========")
-  oll_onemax.reset()
-  oll_onemax.step(1)
-  oll_onemax.close()
+
+  with open('trace.csv', 'w') as traces_file:
+    print('Sample', '|', 'Fitness', file=traces_file, sep='')
+
+    env = OLL_OneMax(traces_file, random_seed = 43)
+    print(env.optimum)
+    observation, info = env.reset()
+
+    for _ in range(1000):
+      action = env.action_space.sample()
+      observation, reward, terminated, truncated, info = env.step(action)
+
+      if terminated or truncated:
+        break
+
+    env.close()
