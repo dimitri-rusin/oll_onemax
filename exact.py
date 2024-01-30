2249c0963413266b57cb6244e410e5e9e0a725cb
Dimitri Rusin <dimitri@habimm.com>
2024-01-30 12:07:35 +0100

    Use AbstractEnv.

--- exact.py
+++ exact.py
@@ -1,3 +1,58 @@
+import dacbench
+import gymnasium
+import numpy
+import onell_algs
+
+class OneMaxOll(dacbench.AbstractEnv):
+  def __init__(self, config):
+    super(OneMaxOll, self).__init__(config)
+    self.n = config['num_dimensions']
+
+    self.random_number_generator = numpy.random.default_rng(config['random_seed'])
+    self.x = onell_algs.OneMax(self.n, rng = self.random_number_generator)
+
+    # We evaluate the onemax function once in the OneMax constructor
+    self.num_evaluations = 1
+    self.num_steps = 0
+
+  def reset_(self):
+    self.state = 0
+    return numpy.array([self.state])
+
+  def reset(self):
+    super().reset_()
+    return self.reset_()
+
+  def step(self, lambda_):
+    p = lambda_ / self.n
+    population_size = round(lambda_)
+    xprime, f_xprime, ne1 = self.x.mutate(p, population_size, self.random_number_generator)
+
+    c = 1 / lambda_
+    y, f_y, ne2 = self.x.crossover(
+      xprime,
+      c,
+      population_size,
+      True,
+      True,
+      self.random_number_generator,
+    )
+
+    prior_x = self.x
+    if f_y >= self.x.fitness:
+      self.x = y
+
+    self.num_evaluations = self.num_evaluations + ne1 + ne2
+    self.num_steps += 1
+
+    reward = self.x.fitness - prior_x.fitness
+    done = self.x.is_optimal()
+    info = {}
+
+    return numpy.array([self.x.fitness]), reward, done, info
+
+
+
 
 import json
 import numpy
@@ -33,12 +88,27 @@ keys = ['theory_lambdas', 'all_one_lambdas', 'all_n_lambdas', 'all_n_half_lambda
 
 for key in keys:
   for random_seed in random_seeds:
-    onell_lambda_result = onell_algs.onell_lambda(
-      n = num_dimensions,
-      lbds = policies[key]['parameters'],
-      seed = random_seed,
-    )
-    policies[key]['num_iteration_samples'].append(onell_lambda_result[2])
+
+    config = {
+      "random_seed": random_seed,
+      "num_dimensions": num_dimensions,
+
+      # not used, required for AbstractEnv
+      "action_space": gymnasium.spaces.Discrete(10),
+      "benchmark_info": "Simple Increment Environment",
+      "cutoff": 100,
+      "instance_set": {'example_instance': None},
+      "observation_space": gymnasium.spaces.Discrete(100),
+      "reward_range": (0, 1),
+    }
+    env = OneMaxOll(config)
+    observation = env.reset()
+    done = False
+    while not done:
+      lambda_ = policies[key]['parameters'][env.x.fitness]
+      observation, reward, done, info = env.step(lambda_)
+      print(f"State: {observation}, Action: {lambda_}, Reward: {reward}, Done: {done}")
+    policies[key]['num_iteration_samples'].append(env.num_evaluations)
 
 print(json.dumps(policies, indent = 2))
 
