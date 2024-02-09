cec88abe8011245ef8998ab235745b2191eaa2fe
Dimitri Rusin <dimitri@habimm.com>
2024-02-09 14:15:05 +0100

    Log from the environment.

--- run_ppo.py
+++ run_ppo.py
@@ -1,84 +1,39 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common.callbacks import BaseCallback
 import csv
 import inspectify
 import numpy
 import os
+import sys
 import torch
 import tuning_environments
+import yaml
 
 
 
-class PolicyLoggerCallback(BaseCallback):
-  def __init__(self, policy_omitted_steps, episode_steps_omitted_steps, filename):
-    super(PolicyLoggerCallback, self).__init__()
-    self.counter = 0
-    self.episode_steps_omitted_steps = episode_steps_omitted_steps
-    self.filename = filename
-    self.num_policies = 0
-    self.policy_omitted_steps = policy_omitted_steps
+try:
+  with open(".env.yaml") as file:
+    config = yaml.safe_load(file)
+except FileNotFoundError:
+  print("Error: '.env.yaml' does not exist.", file = sys.stderr)
+  os._exit(1)
 
-  def _on_step(self) -> bool:
-    self.counter += 1
-
-    # SAVE TRAINING EPISODES
-    if self.counter % self.episode_steps_omitted_steps == 0:
-      with open('ppo_data/episodes.csv', 'a', newline='') as file:
-        already_arrived = any(self.locals['dones'])
-        num_episodes = len(self.locals['env'].envs[0].episode_lengths) + 1
-        total_steps = self.locals['env'].envs[0].total_steps
-        num_steps = total_steps - sum(self.locals['env'].envs[0].episode_lengths)
-        if already_arrived:
-          num_episodes -= 1
-          num_steps += self.locals['env'].envs[0].episode_lengths[-1]
-        fitness = self.locals['obs_tensor'].item()
-        lambda_ = self.locals['clipped_actions'].item()
-        row = total_steps,num_episodes, num_steps, fitness, lambda_
-        writer = csv.writer(file, delimiter='|')
-        writer.writerow(row)
-
-    # SAVE TRAINED POLICY
-    if self.counter % self.policy_omitted_steps == 0:
-      self.num_policies += 1
-      low = int(self.locals['env'].envs[0].env.observation_space.low[0])
-      high = int(self.locals['env'].envs[0].env.observation_space.high[0])
-      fitnesses = numpy.arange(low, high).reshape(-1, 1)
-      lambdas, _ = self.model.predict(fitnesses, deterministic=True)
-      lambdas_1d = lambdas.flatten().tolist()
-      lambdas_1d = [self.num_policies] + lambdas_1d
-      with open('ppo_data/policies.csv', 'a', newline='') as file:
-        writer = csv.writer(file, delimiter='|')
-        writer.writerow(lambdas_1d)
-
-    return True
-
-
-
-config = {
-  'num_dimensions': 10,
-  'random_seed': 12345,
-}
 environment = tuning_environments.OneMaxOll(config)
-model = PPO(
+agent = PPO(
   "MlpPolicy",
   environment,
-  # learning_rate=0.0003,
-  # policy_kwargs=dict(net_arch=dict(pi=[64, 64], vf=[64, 64])),
   verbose=1,
   seed=config['random_seed'],
 )
 
+environment.set_agent(agent)
+
 if not os.path.exists('ppo_data/'):
   os.makedirs('ppo_data/')
 
-
-
-
-
 # SAVE POLICY TRAINING EPISODES, TRAINED POLICY, AND TRAINING ARCHITECTURE
 with open("ppo_data/agent_architecture.json", "w") as arch_file:
-  print(model.policy, file = arch_file)
+  print(agent.policy, file = arch_file)
 low = int(environment.observation_space.low[0])
 high = int(environment.observation_space.high[0])
 fitnesses = numpy.arange(low, high).reshape(-1, 1)
@@ -88,37 +43,18 @@ fitnesses_1d = ['ID'] + fitnesses_1d
 with open('ppo_data/policies.csv', 'w', newline='') as file:
   writer = csv.writer(file, delimiter='|')
   writer.writerow(fitnesses_1d)
-with open('ppo_data/episodes.csv', 'w', newline='') as file:
-  writer = csv.writer(file, delimiter='|')
-  writer.writerow(['Step across episodes', 'Episode', 'Step','Fitness','Lambda'])
 
-
-
-policy_logger_callback = PolicyLoggerCallback(
-  episode_steps_omitted_steps=100,
-  policy_omitted_steps=5_000,
-  filename="policies.csv",
+agent.learn(
+  total_timesteps = config['total_timesteps'],
 )
 
-model.learn(total_timesteps=100_000, callback=[policy_logger_callback])
-
-
-
-# Save the weights
-weights = model.policy.state_dict()
-
-# Saving the weights to a file
+weights = agent.policy.state_dict()
 torch.save(weights, "ppo_data/model_weights.pth")
 
-
-import torch
-
-# Load the model weights
 model_weights_path = 'ppo_data/model_weights.pth'
 model_weights = torch.load(model_weights_path)
 
 with open("ppo_data/model_weights.txt", "w") as weights_file:
-  # Print the layers and their corresponding weights
   for layer_name, weights in model_weights.items():
     print(f"Layer: {layer_name}", file = weights_file)
     print("Weights:", file = weights_file)
--- tuning_environments.py
+++ tuning_environments.py
@@ -4,6 +4,16 @@ import numpy
 import paper_code.onell_algs
 import random
 import dacbench
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+import csv
+import inspectify
+import numpy
+import os
+import sys
+import torch
+import tuning_environments
+import yaml
 
 
 
@@ -19,8 +29,9 @@ class OneMaxOll(dacbench.AbstractEnv):
     config["instance_set"] = {'example_instance': None}
     config["observation_space"] = gymnasium.spaces.Box(low=0, high=self.n, shape=(1,), dtype=numpy.float32)
     config["reward_range"] = (-self.n, 0)
+    self.config = config
 
-    super(OneMaxOll, self).__init__(config)
+    super(OneMaxOll, self).__init__(self.config)
 
     self.random_seed = config['random_seed']
     self.observation_space = config["observation_space"]
@@ -30,10 +41,26 @@ class OneMaxOll(dacbench.AbstractEnv):
     self.x = paper_code.onell_algs.OneMax(self.n, rng = self.random_number_generator)
 
     # We evaluate the onemax function once in the OneMax constructor
+    self.agent = None
+
     self.num_evaluations = 1
+    # There is 1 evaluation in the above paper_code.onell_algs.OneMax().
+
+    self.num_policies = 0
+    self.num_resets = 0
     self.num_steps = 0
+    self.num_steps_across_resets = 0
+
+    with open('ppo_data/episodes.csv', 'w', newline='') as file:
+      writer = csv.writer(file, delimiter='|')
+      writer.writerow(['Step across episodes', 'Episode', 'Step', 'Fitness', 'Lambda'])
+
+  def set_agent(self, agent):
+    self.agent = agent
 
   def reset_(self):
+    self.num_resets += 1
+    self.num_steps = 0
     self.random_number_generator = numpy.random.default_rng(self.random_seed)
     self.x = paper_code.onell_algs.OneMax(self.n, rng = self.random_number_generator)
     return numpy.array([self.x.fitness])
@@ -49,6 +76,7 @@ class OneMaxOll(dacbench.AbstractEnv):
       lambda_ = lambda_.item()
     p = lambda_ / self.n
     population_size = numpy.round(lambda_).astype(int)
+    prior_fitness = self.x.fitness
     xprime, f_xprime, ne1 = self.x.mutate(p, population_size, self.random_number_generator)
 
     c = 1 / lambda_
@@ -61,16 +89,41 @@ class OneMaxOll(dacbench.AbstractEnv):
       self.random_number_generator,
     )
 
-    prior_x = self.x
     if f_y >= self.x.fitness:
       self.x = y
 
     self.num_evaluations = self.num_evaluations + ne1 + ne2
     self.num_steps += 1
+    self.num_steps_across_resets += 1
 
-    reward = -(ne1 + ne2) + (self.x.fitness - prior_x.fitness) + self.x.fitness * lambda_ * 100
+    num_evaluations_of_this_step = ne1 + ne2
+    reward = -num_evaluations_of_this_step + 10 * (self.x.fitness - prior_fitness)
     terminated = self.x.is_optimal()
     truncated = False
     info = {}
+
+    # ======================================================================================
+
+    # SAVE TRAINING EPISODES
+    if self.num_steps_across_resets % self.config['episode_steps_omitted_steps'] == 0:
+      with open('ppo_data/episodes.csv', 'a', newline = '') as file:
+        row = self.num_steps_across_resets, self.num_resets + 1, self.num_steps, prior_fitness, lambda_, reward, self.x.fitness, num_evaluations_of_this_step
+        writer = csv.writer(file, delimiter = '|')
+        writer.writerow(row)
+
+    # SAVE TRAINED POLICY
+    if self.num_steps_across_resets % self.config['policy_omitted_steps'] == 0:
+      self.num_policies += 1
+      low = int(self.config['observation_space'].low[0])
+      high = int(self.config['observation_space'].high[0])
+      fitnesses = numpy.arange(low, high).reshape(-1, 1)
+      lambdas, _ = self.agent.predict(fitnesses, deterministic = True)
+      lambdas_1d = lambdas.flatten().tolist()
+      lambdas_1d = [self.num_policies] + lambdas_1d
+      with open('ppo_data/policies.csv', 'a', newline = '') as file:
+        writer = csv.writer(file, delimiter = '|')
+        writer.writerow(lambdas_1d)
+
+    # ======================================================================================
 
     return numpy.array([self.x.fitness]), reward, terminated, truncated, info
