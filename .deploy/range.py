c126e23cd41dc7d0a20f033f4a37d324abe35d08
Dimitri Rusin <dimitri@habimm.com>
2024-03-17 09:51:12 +0100

    Work with adding together several grids.

--- .deploy/range.py
+++ .deploy/range.py
@@ -16,36 +16,50 @@ def generate_filename_from_config(single_config, wordlist, num_words):
         words.append(wordlist[index])
     return '_'.join(words)
 
-def write_config_to_yaml(directory, config, wordlist, num_words):
-    keys, values = zip(*config.items())
-    for combination in itertools.product(*values):
-        single_config = dict(zip(keys, combination))
-        filename = generate_filename_from_config(single_config, wordlist, num_words)
-        single_config["db_path"] = single_config["db_path"].replace("<wordhash>", filename)
-        yaml_content = yaml.dump(single_config)
-        filepath = os.path.join(directory, f'{filename}.yaml')
+def write_config_to_yaml(directory, configs, wordlist, num_words):
+    for config in configs:
+        keys, values = zip(*config.items())
+        for combination in itertools.product(*values):
+            single_config = dict(zip(keys, combination))
+            filename = generate_filename_from_config(single_config, wordlist, num_words)
+            single_config["db_path"] = single_config["db_path"].replace("<wordhash>", filename)
+            yaml_content = yaml.dump(single_config)
+            filepath = os.path.join(directory, f'{filename}.yaml')
 
-        with open(filepath, 'w') as file:
-            file.write(yaml_content)
+            with open(filepath, 'w') as file:
+                file.write(yaml_content)
 
-# Define the configuration dictionary with lists of possible values
-config = {
-  "db_path": ["computed/data/<wordhash>.db"],
-  "episodes": [999_999_999_999],
-  "epsilon": [0.1],
-  "evaluation_interval": [10],
-  "gamma": [1, 0.99, 0.995, 0.9998],
-  "learning_rate": [0.1],
-  "n": [50, 500, 1_000],
-  "num_evaluation_episodes": [200],
-  "num_training_timesteps": [1_000_000],
-  "probability_of_closeness_to_optimum": [0.5, 0.9],
-  "random_seed": [42]
-}
+# Define the configurations list with possible values
+configs = [
+  {
+    "db_path": ["computed/data/<wordhash>.db"],
+    "episodes": [999_999_999_999],
+    "epsilon": [0.1],
+    "evaluation_interval": [10],
+    "gamma": [1, 0.99, 0.995, 0.9998],
+    "learning_rate": [0.1],
+    "n": [50],
+    "num_evaluation_episodes": [200],
+    "num_training_timesteps": [1_000_000],
+    "probability_of_closeness_to_optimum": [0.5, 0.9],
+    "random_seed": [42]
+  },
+  {
+    "db_path": ["computed/data/<wordhash>.db"],
+    "episodes": [999_999_999_999],
+    "epsilon": [0.1],
+    "evaluation_interval": [10],
+    "gamma": [1, 0.99, 0.995, 0.9998],
+    "learning_rate": [0.1],
+    "n": [500, 1_000],
+    "num_evaluation_episodes": [25],
+    "num_training_timesteps": [1_000_000],
+    "probability_of_closeness_to_optimum": [0.5, 0.9],
+    "random_seed": [42]
+  },
+]
 
 wordlist = load_wordlist('.deploy/eff_large_wordlist.txt')
-
 directory = 'config/March_11'
 os.makedirs(directory, exist_ok=True)
-
-write_config_to_yaml(directory, config, wordlist, num_words=5)
+write_config_to_yaml(directory, configs, wordlist, num_words=5)
