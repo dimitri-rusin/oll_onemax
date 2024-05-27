3da472558f047f035d40534606724826c60a2c43
Dimitri Rusin <dimitri@habimm.com>
2024-05-27 21:33:21 +0200

    Draw 'config' out of the called functions.

--- src/generate.py
+++ src/generate.py
@@ -15,26 +15,23 @@ import_path = str(pathlib.Path(__file__).resolve().parent.parent)
 sys.path.append(import_path)
 import DE0CH_OLL.tuned_with_irace.onell_algs
 
-config = None
-
 # ============== ENVIRONMENT - BEGIN ==============
 
 class OneMaxOLL(gymnasium.Env):
-  def __init__(self, dimensionality, seed, reward_type, action_type, state_type):
+  def __init__(self, dimensionality, seed, reward_type, action_type, state_type, config):
     super(OneMaxOLL, self).__init__()
     self.dimensionality = dimensionality
+    self.config = config
 
-    if 'lambdas' in config:
-      self.lambdas = numpy.array(config['lambdas'], dtype = numpy.float64)
-
+    if 'lambdas' in self.config:
+      self.lambdas = numpy.array(self.config['lambdas'], dtype = numpy.float64)
       self.action_space = gymnasium.spaces.Discrete(self.lambdas.shape[0])
 
-    if 'mutation_rates' in config:
-      self.mutation_rates = numpy.array(config['mutation_rates'], dtype = numpy.float64)
-      self.mutation_sizes = numpy.array(config['mutation_sizes'], dtype = numpy.int64)
-      self.crossover_rates = numpy.array(config['crossover_rates'], dtype = numpy.float64)
-      self.crossover_sizes = numpy.array(config['crossover_sizes'], dtype = numpy.int64)
-
+    if 'mutation_rates' in self.config:
+      self.mutation_rates = numpy.array(self.config['mutation_rates'], dtype = numpy.float64)
+      self.mutation_sizes = numpy.array(self.config['mutation_sizes'], dtype = numpy.int64)
+      self.crossover_rates = numpy.array(self.config['crossover_rates'], dtype = numpy.float64)
+      self.crossover_sizes = numpy.array(self.config['crossover_sizes'], dtype = numpy.int64)
       self.action_space = gymnasium.spaces.MultiDiscrete([
         self.mutation_rates.shape[0],
         self.mutation_sizes.shape[0],
@@ -49,7 +46,7 @@ class OneMaxOLL(gymnasium.Env):
     self.state_type = state_type
 
     self.seed = seed
-    self.random_number_generator = numpy.random.default_rng(self.seed)
+    self.random_number_generator = numpy.random.Generator(numpy.random.MT19937(self.seed))
     self.current_solution = None
     self.num_function_evaluations = None
     self.num_total_timesteps = 0
@@ -60,11 +57,11 @@ class OneMaxOLL(gymnasium.Env):
     if seed is not None:
       self.seed = seed
     self.num_function_evaluations = 0
-    self.random_number_generator = numpy.random.default_rng(self.seed)
+    self.random_number_generator = numpy.random.Generator(numpy.random.MT19937(self.seed))
 
     self.current_fitness = self.dimensionality
     while self.current_fitness >= self.dimensionality:
-      ratio_of_optimal_bits = config['closeness_to_optimum']
+      ratio_of_optimal_bits = self.config['closeness_to_optimum']
       self.current_bitstring = self.random_number_generator.choice(
         [True, False],
         size = self.dimensionality,
@@ -78,13 +75,13 @@ class OneMaxOLL(gymnasium.Env):
     if self.state_type == 'SCALAR_ENCODED':
       fitness_array = numpy.array([self.current_fitness])
     if self.state_type == 'ONE_HOT_ENCODED':
-      fitness_array = create_fitness_vector(self.current_fitness)
+      fitness_array = create_fitness_vector(self.current_fitness, self.dimensionality)
 
     return fitness_array, {}
 
   def step(self, action_index):
 
-    if 'lambdas' in config:
+    if 'lambdas' in self.config:
       # action_index is of type: <class 'numpy.int64'>
       lambda_ = self.lambdas[action_index]
       mutation_rate = numpy.float64(lambda_ / self.dimensionality)
@@ -92,14 +89,14 @@ class OneMaxOLL(gymnasium.Env):
       crossover_rate = numpy.float64(1.0 / lambda_)
       crossover_size = numpy.int64(lambda_)
 
-    if 'mutation_rates' in config:
+    if 'mutation_rates' in self.config:
       # action_index is of type: <class 'numpy.ndarray'> (with 4 elements)
       mutation_rate = self.mutation_rates[action_index[0]]
       mutation_size = self.mutation_sizes[action_index[1]]
       crossover_rate = self.crossover_rates[action_index[2]]
       crossover_size = self.crossover_sizes[action_index[3]]
 
-    generation_seed = numpy.random.randint(int(1e9))
+    generation_seed = self.random_number_generator.integers(int(1e9))
 
     next_bitstring, num_function_evaluations_of_this_step = onell_algs_rs.generation_full_py(
       self.current_bitstring.tolist(),
@@ -134,13 +131,13 @@ class OneMaxOLL(gymnasium.Env):
     if self.state_type == 'SCALAR_ENCODED':
       fitness_array = numpy.array([self.current_fitness])
     if self.state_type == 'ONE_HOT_ENCODED':
-      fitness_array = create_fitness_vector(self.current_fitness)
+      fitness_array = create_fitness_vector(self.current_fitness, self.dimensionality)
 
     return fitness_array, reward, terminated, truncated, info
 
 # ============== ENVIRONMENT - END ==============
 
-def evaluate_episode(size_policy, episode_seed):
+def evaluate_episode(size_policy, episode_seed, closeness_to_optimum):
 
   oll_parameters = [parameter_tuple for _, parameter_tuple in size_policy.items()]
   dimensionality = len(size_policy)
@@ -149,19 +146,18 @@ def evaluate_episode(size_policy, episode_seed):
     oll_parameters,
     episode_seed,
     int(1e9),
-    config['closeness_to_optimum'],
+    closeness_to_optimum,
   )
 
   return num_function_evaluations, num_evaluation_timesteps
 
-def create_fitness_vector(fitness):
-  fitness_vector = numpy.zeros(config['dimensionality'], dtype=numpy.int32)
-  if fitness < config['dimensionality']:
+def create_fitness_vector(fitness, dimensionality):
+  fitness_vector = numpy.zeros(dimensionality, dtype=numpy.int32)
+  if fitness < dimensionality:
     fitness_vector[fitness] = 1
   return fitness_vector
 
 def load_config():
-  global config
   config = {}
   for key, value in os.environ.items():
     if key.startswith("OO__"):
@@ -203,6 +199,7 @@ def load_config():
           d[part] = {}
         d = d[part]
       d[key_parts[-1]] = parsed_value
+  return config
 
 def flatten_config(prefix, nested_config):
   items = []
@@ -213,13 +210,6 @@ def flatten_config(prefix, nested_config):
     else:
       items.append((new_key, value))
   return items
-
-
-
-
-
-
-
 
 def evaluate_policy(
   crossover_rates,
@@ -233,49 +223,49 @@ def evaluate_policy(
   num_training_timesteps,
   ppo_agent,
   state_type,
+  closeness_to_optimum,
+  episode_seed_generator,
 ):
   if state_type == 'ONE_HOT_ENCODED':
-    index_policy = {int(obs_value): ppo_agent.predict(create_fitness_vector(obs_value), deterministic=True)[0].tolist() for obs_value in range(dimensionality)}
+    index_policy = {int(obs_value): ppo_agent.predict(create_fitness_vector(obs_value, dimensionality), deterministic=True)[0].tolist() for obs_value in range(dimensionality)}
   if state_type == 'SCALAR_ENCODED':
     index_policy = {int(obs_value): ppo_agent.predict(numpy.array([obs_value]).reshape((1, 1)), deterministic=True)[0].tolist() for obs_value in range(dimensionality)}
 
   size_policy = {}
 
-  if 'lambdas' in config:
+  if lambdas is not None:
     for fitness, lambda_index in index_policy.items():
       lambda_ = lambdas[lambda_index]
       mutation_rate = lambda_ / dimensionality
       crossover_rate = 1.0 / lambda_
       size_policy[fitness] = (numpy.float64(mutation_rate), numpy.int64(lambda_), numpy.float64(crossover_rate), numpy.int64(lambda_))
 
-  if 'mutation_rates' in config:
+  if mutation_rates is not None:
     for fitness, (mutation_rate_index, mutation_size_index, crossover_rate_index, crossover_size_index) in index_policy.items():
       size_policy[fitness] = (mutation_rates[mutation_rate_index], int(mutation_sizes[mutation_size_index]), crossover_rates[crossover_rate_index], int(crossover_sizes[crossover_size_index]))
 
   with sqlite3.connect(db_path) as database:
     cursor = database.cursor()
     current_time = datetime.datetime.now(datetime.timezone.utc).isoformat()
-    cursor.execute('INSERT INTO CONSTRUCTED_POLICIES (num_total_timesteps, created_at) VALUES (?, ?);', (num_training_timesteps, current_time))
+    cursor.execute('INSERT INTO CONSTRUCTED_POLICIES (num_total_timesteps, created_at) VALUES (?, ?)', (num_training_timesteps, current_time))
     policy_id = cursor.lastrowid
     cursor.executemany(
-      'INSERT INTO POLICY_DETAILS (policy_id, fitness, mutation_rate, mutation_size, crossover_rate, crossover_size) VALUES (?, ?, ?, ?, ?, ?);',
+      'INSERT INTO POLICY_DETAILS (policy_id, fitness, mutation_rate, mutation_size, crossover_rate, crossover_size) VALUES (?, ?, ?, ?, ?, ?)',
       [(policy_id, fitness, mutation_rate, mutation_size, crossover_rate, crossover_size) for fitness, (mutation_rate, mutation_size, crossover_rate, crossover_size) in size_policy.items()]
     )
 
-  seed_for_generating_episode_seeds = numpy.random.randint(int(1e9))
-  episode_seed_generator = numpy.random.RandomState(seed_for_generating_episode_seeds)
   num_function_evaluations_list = []
 
   for episode_index in range(num_evaluation_episodes):
     print(f"Policy {policy_id}: Evaluating episode {episode_index + 1:,} / {num_evaluation_episodes:,}.")
-    episode_seed = episode_seed_generator.randint(int(1e9))
-    num_function_evaluations, num_evaluation_timesteps = evaluate_episode(size_policy, episode_seed)
+    episode_seed = episode_seed_generator.integers(int(1e9))
+    num_function_evaluations, num_evaluation_timesteps = evaluate_episode(size_policy, episode_seed, closeness_to_optimum)
     num_function_evaluations_list.append((policy_id, episode_seed, num_evaluation_timesteps, num_function_evaluations))
 
   with sqlite3.connect(db_path, timeout=10) as database:
     cursor = database.cursor()
     cursor.executemany(
-      'INSERT INTO EVALUATION_EPISODES (policy_id, episode_seed, num_evaluation_timesteps, num_function_evaluations) VALUES (?, ?, ?, ?);',
+      'INSERT INTO EVALUATION_EPISODES (policy_id, episode_seed, num_evaluation_timesteps, num_function_evaluations) VALUES (?, ?, ?, ?)',
       num_function_evaluations_list
     )
 
@@ -286,11 +276,19 @@ def evaluate_policy(
 
 
 
+
+
+
+
+
+
+
 def main():
+  config = load_config()
 
-  load_config()
-
-  numpy.random.seed(config['random_seed'])
+  main_generator_seed = config['random_seed']
+  mersenne_twister = numpy.random.MT19937(main_generator_seed)
+  main_generator = numpy.random.Generator(mersenne_twister)
 
   if os.path.isfile(config['db_path']):
     os.remove(config['db_path'])
@@ -333,18 +331,18 @@ def main():
   with database:
     cursor = database.cursor()
     if policy_id is None:
-      cursor.execute('INSERT INTO CONSTRUCTED_POLICIES (num_training_episodes) VALUES (?);', (0,))
+      cursor.execute('INSERT INTO CONSTRUCTED_POLICIES (num_training_episodes) VALUES (?)', (0,))
       policy_id = cursor.lastrowid
     else:
       cursor.execute(
         'INSERT INTO CONSTRUCTED_POLICIES '
         '(policy_id, num_total_timesteps, num_training_episodes, '
         'num_total_function_evaluations, mean_initial_fitness, variance_initial_fitness) '
-        'VALUES (?, ?, ?, ?, ?, ?);',
+        'VALUES (?, ?, ?, ?, ?, ?)',
         (policy_id, 0, 0, 0, 0, 0)
       )
       cursor.executemany(
-        'INSERT INTO POLICY_DETAILS (policy_id, fitness, mutation_rate, mutation_size, crossover_rate, crossover_size) VALUES (?, ?, ?, ?, ?, ?);',
+        'INSERT INTO POLICY_DETAILS (policy_id, fitness, mutation_rate, mutation_size, crossover_rate, crossover_size) VALUES (?, ?, ?, ?, ?, ?)',
         [(policy_id, fitness, mutation_rate, mutation_size, crossover_rate, crossover_size) for fitness, (mutation_rate, mutation_size, crossover_rate, crossover_size) in theory_derived_size_policy.items()]
       )
 
@@ -352,20 +350,18 @@ def main():
   num_evaluation_episodes = config['num_evaluation_episodes']
 
 
-  seed_for_generating_episode_seeds = numpy.random.randint(int(1e9))
-  episode_seed_generator = numpy.random.RandomState(seed_for_generating_episode_seeds)
   episode_data = []
   for episode_index in range(num_evaluation_episodes):
     print(f"Policy {policy_id}: Evaluating episode {episode_index + 1:,} / {num_evaluation_episodes:,}.")
-    episode_seed = episode_seed_generator.randint(int(1e9))
-    num_function_evaluations, num_evaluation_timesteps = evaluate_episode(theory_derived_size_policy, episode_seed)
+    episode_seed = main_generator.integers(int(1e9))
+    num_function_evaluations, num_evaluation_timesteps = evaluate_episode(theory_derived_size_policy, episode_seed, config['closeness_to_optimum'])
     episode_data.append((policy_id, episode_seed, num_evaluation_timesteps, num_function_evaluations))
 
   with sqlite3.connect(db_path, timeout=10) as database:
     with database:
       cursor = database.cursor()
       cursor.executemany(
-        'INSERT INTO EVALUATION_EPISODES (policy_id, episode_seed, num_evaluation_timesteps, num_function_evaluations) VALUES (?, ?, ?, ?);',
+        'INSERT INTO EVALUATION_EPISODES (policy_id, episode_seed, num_evaluation_timesteps, num_function_evaluations) VALUES (?, ?, ?, ?)',
         episode_data
       )
 
@@ -409,22 +405,25 @@ def main():
         num_training_timesteps = self.num_timesteps,
         ppo_agent = self.model,
         state_type = config['state_type'],
+        closeness_to_optimum = config['closeness_to_optimum'],
+        episode_seed_generator = main_generator,
       )
 
       return True
 
-  sb3_seed = numpy.random.randint(int(1e9))
+  sb3_seed = main_generator.integers(int(1e9))
   stable_baselines3.common.utils.set_random_seed(sb3_seed)
 
   # Create a function to instantiate the environment with different seeds
   def create_env():
-    seed = numpy.random.randint(int(1e9))
+    seed = main_generator.integers(int(1e9))
     return OneMaxOLL(
-      dimensionality=dimensionality,
-      seed=seed,
-      state_type=config['state_type'],
-      action_type=config['action_type'],
-      reward_type=config['reward_type']
+      dimensionality = dimensionality,
+      seed = seed,
+      state_type = config['state_type'],
+      action_type = config['action_type'],
+      reward_type = config['reward_type'],
+      config = config,
     )
 
   environments = stable_baselines3.common.env_util.make_vec_env(create_env, n_envs=config['num_environments'])
@@ -457,11 +456,11 @@ def main():
     num_training_timesteps = 0,
     ppo_agent = ppo_agent,
     state_type = config['state_type'],
+    closeness_to_optimum = config['closeness_to_optimum'],
+    episode_seed_generator = main_generator,
   )
 
   ppo_agent.learn(total_timesteps=config['max_training_timesteps'], callback=PPOCallback())
-
-
 
 if __name__ == '__main__':
   main()
