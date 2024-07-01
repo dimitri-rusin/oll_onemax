55cce7cec21b7530f93bd8ad08a686eb81523072
Dimitri Rusin <dimitri@habimm.com>
2024-07-01 18:33:45 +0200

    Rounding

--- src/rounding.py
+++ src/rounding.py
@@ -1,4 +1,5 @@
 import json
+import inspectify
 import numpy
 import onell_algs_rs
 import os
@@ -9,20 +10,20 @@ def simplify(policy, action_space, decision):
     if value in action_space:
       simplified_policy[key] = value
     else:
-      if decision == "smaller":
+      if decision == "rounded_down_policy":
         smaller_nums = [x for x in action_space if x < value]
         simplified_policy[key] = max(smaller_nums) if smaller_nums else value
-      elif decision == "greater":
+      elif decision == "rounded_up_policy":
         greater_nums = [x for x in action_space if x > value]
         simplified_policy[key] = min(greater_nums) if greater_nums else value
   return simplified_policy
 
 def generate_two_simplified_policies(policy, action_space):
-  smaller_policy = simplify(policy, action_space, "smaller")
-  greater_policy = simplify(policy, action_space, "greater")
-  return [('smaller_policy', smaller_policy), ('greater_policy', greater_policy)]
+  rounded_down_policy = simplify(policy, action_space, "rounded_down_policy")
+  rounded_up_policy = simplify(policy, action_space, "rounded_up_policy")
+  return [('rounded_down_policy', rounded_down_policy), ('rounded_up_policy', rounded_up_policy)]
 
-def evaluate(policy, closeness_to_optimum, main_generator):
+def evaluate(policy, closeness_to_optimum, episode_seed):
 
   dimensionality = len(policy)
 
@@ -33,62 +34,73 @@ def evaluate(policy, closeness_to_optimum, main_generator):
     crossover_rate = 1.0 / lambda_
     four_parameters = (
       numpy.float64(mutation_rate),
-      numpy.int64(lambda_),
+      numpy.int64(numpy.around(lambda_)),
       numpy.float64(crossover_rate),
-      numpy.int64(lambda_),
+      numpy.int64(numpy.around(lambda_)),
     )
     parameters_policy.append(four_parameters)
 
-  episode_seed = main_generator.integers(int(1e9))
   num_function_evaluations, num_evaluation_timesteps = onell_algs_rs.onell_lambda(
-    dimensionality,
-    parameters_policy,
-    episode_seed,
-    int(1e9),
-    closeness_to_optimum,
+    n = dimensionality,
+    oll_parameters = parameters_policy,
+    seed = episode_seed,
+    max_evals = int(1e9),
+    probability = closeness_to_optimum,
   )
   return num_function_evaluations, num_evaluation_timesteps
 
-def statistics(dimensionality, closeness_to_optimum, precision, seed, filepath):
+def statistics(dimensionalities, closeness_to_optimum, precision, seed, filepath):
 
-  action_space = [2 ** i for i in range(int(numpy.log2(dimensionality)))]
-  theory_derived_size_policy = {
-    fitness: int(numpy.sqrt(dimensionality / (dimensionality - fitness))) \
-    for fitness in range(dimensionality)
-  }
-  simplified_policies = generate_two_simplified_policies(theory_derived_size_policy, action_space)
-  policies = [('theory_derived_size_policy', theory_derived_size_policy)] + simplified_policies
-
-  main_generator = numpy.random.Generator(numpy.random.MT19937(seed))
   policies_info = []
-  for name, policy in policies:
-    count = 0
-    sum_num_function_evaluations = 0
-    sum_num_evaluation_timesteps = 0
-    for _ in range(precision):
-      num_function_evaluations, num_evaluation_timesteps = evaluate(
-        policy = policy,
-        closeness_to_optimum = closeness_to_optimum,
-        main_generator = main_generator,
-      )
-      count += 1
-      sum_num_function_evaluations += num_function_evaluations
-      sum_num_evaluation_timesteps += num_evaluation_timesteps
-
-    avg_num_function_evaluations = sum_num_function_evaluations / count
-    avg_num_evaluation_timesteps = sum_num_evaluation_timesteps / count
-    policy_info = {
-      'seed': seed,
-      'closeness_to_optimum': closeness_to_optimum,
-
-      'policy': name,
-      'dimensionality': dimensionality,
-      'count': count,
-      'avg_num_evaluation_timesteps': avg_num_evaluation_timesteps,
-      'avg_num_function_evaluations': avg_num_function_evaluations,
+  for dimensionality in dimensionalities:
+    action_space = [2 ** i for i in range(int(numpy.log2(dimensionality)))]
+    theory_derived_policy = {
+      fitness: numpy.sqrt(dimensionality / (dimensionality - fitness)) \
+      for fitness in range(dimensionality)
     }
+    simplified_policies = generate_two_simplified_policies(theory_derived_policy, action_space)
+    policies = [('theory_derived_policy', theory_derived_policy)] + simplified_policies
 
-    policies_info.append(policy_info)
+    for name, policy in policies:
+      count = 0
+      sum_num_function_evaluations = 0
+      sum_num_evaluation_timesteps = 0
+      sum_squares_evaluations = 0
+      sum_squares_timesteps = 0
+
+      main_generator = numpy.random.Generator(numpy.random.MT19937(seed))
+      for _ in range(precision):
+        episode_seed = main_generator.integers(int(1e9))
+        num_function_evaluations, num_evaluation_timesteps = evaluate(
+          policy = policy,
+          closeness_to_optimum = closeness_to_optimum,
+          episode_seed = episode_seed,
+        )
+        count += 1
+        sum_num_function_evaluations += num_function_evaluations
+        sum_num_evaluation_timesteps += num_evaluation_timesteps
+        sum_squares_evaluations += num_function_evaluations ** 2
+        sum_squares_timesteps += num_evaluation_timesteps ** 2
+
+      avg_num_function_evaluations = sum_num_function_evaluations / count
+      avg_num_evaluation_timesteps = sum_num_evaluation_timesteps / count
+      std_dev_evaluations = numpy.sqrt(sum_squares_evaluations / count - avg_num_function_evaluations ** 2)
+      std_dev_timesteps = numpy.sqrt(sum_squares_timesteps / count - avg_num_evaluation_timesteps ** 2)
+
+      policy_info = {
+        'dimensionality': dimensionality,
+        'policy': name,
+
+        'seed': seed,
+        'count': count,
+        'avg_num_evaluation_timesteps': avg_num_evaluation_timesteps,
+        'std_dev_evaluation_timesteps': std_dev_timesteps,
+        'avg_num_function_evaluations': avg_num_function_evaluations,
+        'std_dev_function_evaluations': std_dev_evaluations,
+        'closeness_to_optimum': closeness_to_optimum,
+      }
+
+      policies_info.append(policy_info)
 
   with open(filepath, 'w') as json_file:
     print(json.dumps(policies_info, indent = 2), file = json_file)
@@ -98,9 +110,9 @@ def statistics(dimensionality, closeness_to_optimum, precision, seed, filepath):
 
 
 policies_info = statistics(
-  dimensionality = 500,
+  dimensionalities = [12, 15],
   seed = 42,
-  precision = 50,
+  precision = 100,
   closeness_to_optimum = 0.5,
-  filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'policies_info.json'),
+  filepath = 'computed/policies_info.json',
 )
